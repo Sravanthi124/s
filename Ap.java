66b0c726dc0539d87adb008ebc2c728d6d22af72
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2019-06-03 18:03:35 +0530

    Update Ap.java

--- Ap.java
+++ Ap.java
@@ -5,11 +5,12 @@ public class Ap {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		int N=sc.nextInt();
-		int A=sc.nextInt();
-		int D=sc.nextInt();
-		int sum=(N / 2) * (2 * A + (N - 1) * D) ;
+		float N=sc.nextFloat();
+		float A=sc.nextFloat();
+		float D=sc.nextFloat();
+		float sum=(float)(N / 2) * (2 * A + (N - 1) * D) ;
 		System.out.println(sum);
 	}
 
 }
+
