d74db79ed333966a697c4b2db193cebe545b548b
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2019-06-25 11:13:09 +0530

    Create Difference_character.java

---
+++ Difference_character.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Difference_character {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String st=sc.nextLine();
+		String st1=sc.nextLine();
+		int count=0;
+		if(st.length()!=st1.length())
+		{
+			System.out.println("No");
+		}
+		else
+		{
+			for(int i=0;i<st.length();i++)
+			{
+				if(st.charAt(i)!=st1.charAt(i))
+				{
+					count++;
+				}
+			}
+			if(count==1)
+				System.out.println("Yes");
+			else
+				System.out.println("No");
+		}
+	}
+
+}
