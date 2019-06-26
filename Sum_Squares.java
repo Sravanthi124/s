64d17ba5e2ec6d30d285d8b26bd5f9376a05bed0
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2019-06-26 13:47:53 +0530

    Create Sum_Squares.java

---
+++ Sum_Squares.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Sum_Squares {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
+		while(n>0)
+		{
+			int a=n%10;
+			sum=sum+(a*a);
+			n=n/10;
+		}
+		System.out.println(sum);
+	}
+
+}
